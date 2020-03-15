b8870e89abf26c71c6278cae14dcaae05379353f
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 03:53:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,10 +2,19 @@ class School
   attr_accessor :roster
   @@roster={}
   
-  
   def initialize(school)
     @school = school
     @roster = []
+  end
+
+  def add_student
+  end
+
+  def grade
+  end
+  
+  def sort
+    @roster.sort
   end
 
 end
