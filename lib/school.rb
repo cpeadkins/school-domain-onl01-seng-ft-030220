b1b759d052029c491b833704e09a3cae5679da79
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 03:54:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,14 +7,22 @@ class School
     @roster = []
   end
 
-  def add_student
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
   end
 
-  def grade
+  def grade(grade)
+    @roster[grade]
   end
-  
+
   def sort
-    @roster.sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
   end
 
 end
